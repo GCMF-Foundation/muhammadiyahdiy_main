e4202f814fc796ea9baa7b63148203150962e61e
Auzan <auzan.ip@gmail.com>
2022-03-08 12:09:46 +0700

    update

--- src/layouts/PageLayout/index.js
+++ src/layouts/PageLayout/index.js
@@ -1,42 +1,38 @@
-
 //NOT EDITABLE!!!
-import Topography from "../../components/Topography"
-import Hero from "../../components/Hero"
-import Footer from "../../components/Footer"
+import Topography from "../../components/Topography";
+import Hero from "../../components/Hero";
+import Footer from "../../components/Footer";
 
 // EDITABLE
-import background from "../../assets/img/background.jpg" //Background
-import logo from '../../assets/img/logo.png'; //Logo
-
+import background from "../../assets/img/background.jpg"; //Background
+import logo from "../../assets/img/logo.png"; //Logo
 
 function pageLayout(props) {
-    //EDITABLE
-    const deskripsi = "Selamat datang kembali di"
-    const title1 = "Muhammadiyah DIY"
-    const title2 = "Blended Learning"
-    const slogan = "Siapa yang menempuh jalan untuk mencari ilmu, maka Allah akan mudahkan baginya jalan menuju surga."
-    const copyright = "2021 Muhammadiyah DIY Blended Learning - Didukung oleh Geschool"
+  //EDITABLE
+  const deskripsi = "Selamat datang kembali di";
+  const title1 = "Muhammadiyah DIY";
+  const title2 = "Blended Learning";
+  const slogan =
+    "Siapa yang menempuh jalan untuk mencari ilmu, maka Allah akan mudahkan baginya jalan menuju surga.";
+  const copyright = "2021 Muhammadiyah DIY Blended Learning - Didukung oleh";
 
-    return (
-        <div>
-            <Topography />
-            <Hero
-                logo={logo}
-                background={background}
-                deskripsi={deskripsi}
-                title1={title1}
-                title2={title2}
-                slogan={slogan}
-                appName={props.appName}
-            >
-                {props.children}
-            </Hero>
-            <Footer
-                copyright={copyright}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Topography />
+      <Hero
+        logo={logo}
+        background={background}
+        deskripsi={deskripsi}
+        title1={title1}
+        title2={title2}
+        slogan={slogan}
+        appName={props.appName}
+      >
+        {props.children}
+      </Hero>
+      <Footer copyright={copyright} />
+    </div>
+  );
 }
-
 
 export default pageLayout;
